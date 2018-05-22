e1a9d3a6904cf5776a87e9fa1a693b84827b2967
Mayowa Atte <mayowa.atte@bluemetal.com>
2018-05-22 11:07:31 -0700

    Initial commit

---
+++ JsonCaseRepro/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonCaseRepro
+{
+    public class Result
+    {
+        public string PropertyOne { get; set; }
+        public int PropertyTwo { get; set; }
+        public string Single { get; set; }
+
+        public Result ComplexProperty { get; set; }
+
+
+    }
+}
